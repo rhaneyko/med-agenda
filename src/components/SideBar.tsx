4e37ea42549526f8d85c25e7d69101538f1c6216
rhaneyko <rhaneykohonorio@gmail.com>
2025-02-15 16:28:46 -0400

    Alterando os links de cada pagina do SideBar para acessar as paginas.

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,6 +1,6 @@
 // src/components/Sidebar.tsx
 import React from "react";
-
+import { Link } from "react-router";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Sidebar: React.FC = () => {
@@ -9,17 +9,24 @@ const Sidebar: React.FC = () => {
       <h4 className="text-center mb-4">Med-Agenda</h4>
       <ul className="nav nav-pills flex-column mb-auto">
         <li className="nav-item">
+          <Link to="/" className="nav-link active">
             🏠 Home
+          </Link>
         </li>
         <li>
-
+          <Link to="/agendamentos" className="nav-link text-dark">
             📅 Agendamentos
+          </Link>
         </li>
         <li>
+          <Link to="/pacientes" className="nav-link text-dark">
             👥 Pacientes
+          </Link>
         </li>
         <li>
+          <Link to="/configuracoes" className="nav-link text-dark">
             ⚙️ Configurações
+          </Link>
         </li>
       </ul>
     </div>
