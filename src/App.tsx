55fff2f206a3b799ba8195fd93e7e7f4932647de
rhaneyko <rhaneykohonorio@gmail.com>
2025-02-15 16:28:55 -0400

    Configuração de rotas.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import Home from "./pages/Home"
+// src/App.tsx
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import Agendamentos from "./pages/Agendamentos";
+import Pacientes from "./pages/Pacientes";
+import Configuracoes from "./pages/Configuracoes";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div>
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="agendamentos" element={<Agendamentos />} />
+          <Route path="pacientes" element={<Pacientes />} />
+          <Route path="configuracoes" element={<Configuracoes />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-  )
-}
-
-export default App
+export default App;
