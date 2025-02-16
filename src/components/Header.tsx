8fa96edf9f4bcd82f1bc204b0e030530ead93011
rhaneyko <rhaneykohonorio@gmail.com>
2025-02-16 16:46:37 -0400

    Criação do component header.

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return(
+        <div className="">
+            <div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Header;
